a3a1f226b4821e4cbf0d55cac4070111eaf3e3b2
tylen <tylencallander@gmail.com>
2024-06-03 11:35:57 -0400

    test

--- kontek_erp_3_project.py
+++ kontek_erp_3_project.py
@@ -6,7 +6,7 @@ basepath = 'P:/CONREC/ARCHIVE'
 serialnums = {}
 errors = {}
 
-# Extracts serial numbers, from the Excel file
+# Extracts serial numbers, from the Excel file Testing
 
 def extract_serial_numbers_from_excel(excel_file_path):
     try:
